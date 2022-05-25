24d9f2a2835c94ea6eefaf67a86527be81dde807
luismch <lh0202275202@gmail.com>
2022-05-25 10:12:12 -0500

    Decomposición

---
+++ segunda practica.py
@@ -0,0 +1,27 @@
+
+class automovil:
+
+    def __init__(self, modelo, marca, color):
+        self.modelo = modelo
+        self.marca = marca
+        self.color = color
+        self._estado = "en reposo"
+        self._motor = motor(cilindros = 4)
+
+    def acelerar(self, tipo= "despacio"):
+        if tipo == "rapida":
+            self._motor.inyecta_gasolina(10) 
+        else:
+            self._motor.inyecta_gasolina(3)
+
+        self._estado = "en_movimiento"
+
+class motor:
+
+    def __init__(self, cilindros, tipo = "gasolina"):
+        self.cilindros = cilindros
+        self.tipo = tipo
+        self._temperatura = 0
+
+    def inyecta_gasolina(self, cantidad):
+        pass
