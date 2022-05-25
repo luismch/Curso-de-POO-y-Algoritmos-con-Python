3d0e2fbdad9a5d1245ec88b4287777ee89415e4a
luismch <lh0202275202@gmail.com>
2022-05-25 11:07:22 -0500

    Herencia

---
+++ quinta practica.py
@@ -0,0 +1,27 @@
+
+class rectangulo:
+
+    def __init__ (self, base , altura):
+        self.base = base
+        self.altura = altura
+
+    def area(self):
+        return self.base * self.altura
+
+
+class cuadrado(rectangulo):
+
+
+    def __init__(self, lado):
+        super().__init__(lado, lado)
+
+
+if __name__ == "__main__":
+    rectangulo = rectangulo(base=3, altura=4)
+    print(rectangulo.area())
+
+    cuadrado = cuadrado(lado=5)
+    print(cuadrado.area())
+
+
+
