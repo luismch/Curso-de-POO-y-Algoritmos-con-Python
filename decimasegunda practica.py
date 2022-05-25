c49562f62fa207c4b2376622889e2fe65bbe2069
luismch <lh0202275202@gmail.com>
2022-05-25 16:07:12 -0500

    Ordenamiento de burbuja

--- decimasegunda practica.py
+++ decimasegunda practica.py
@@ -4,7 +4,7 @@ def ordenamiento_de_burbuja(lista):
     n = len(lista)
 
     for i in range (n):
-        for j in range(0, n - i -1):
+        for j in range(0, n - i -1): # 0(n) * 0(n) = 0(n * n) = 0(n**2)
 
             if lista[j] > lista [j +1]:
                 lista[j], lista[j + 1] = lista[j + 1], lista[j]
