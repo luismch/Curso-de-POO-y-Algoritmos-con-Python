6d381749ad96f508dae2fbb827cc70561d4a6e03
luismch <lh0202275202@gmail.com>
2022-05-26 11:34:02 -0500

    El problema del morral

--- morral.py
+++ morral.py
@@ -15,7 +15,7 @@ def morral(tamano_morral, pesos, valores, n):
 if __name__ == "__main__":
     valores = [60, 100, 120]
     pesos = [10, 20 ,30]
-    tamano_morral = 5
+    tamano_morral = 50
     n = len(valores)
 
     resultado = morral(tamano_morral, pesos, valores, n)
